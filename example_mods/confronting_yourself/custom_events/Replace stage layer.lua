887b990e2ef4696dcd3b23dfa829abcabb034ea0
AltronMaxX <75096038+AltronMaxX@users.noreply.github.com>
2022-10-13 22:57:05 +0400

    delete example mods :)

--- example_mods/confronting_yourself/custom_events/Replace stage layer.lua
+++
@@ -1,8 +0,0 @@
-function onEvent(name, value)
-	if name == 'Replace stage layer' then
-		stage1 = value1;
-        doTweenAlpha('stageAppear', stage1, 1, 0.0001, 'linear');
-		stage2 = value2;
-		doTweenAlpha('stageGone', stage2, 0, 0.0001, 'linear');
-	end
-end
--- example_mods/parendoggle/custom_notetypes/plr4.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-	for i = 0,getProperty('unspawnNotes.length')-1 do
-		if getPropertyFromGroup('unspawnNotes',i,'noteType') == 'plr4' then
-			setPropertyFromGroup('unspawnNotes',i,'noAnimation',true)
-		end
-	end
-end

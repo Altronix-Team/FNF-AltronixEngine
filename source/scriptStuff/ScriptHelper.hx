ed8bb66ea0062faa64a2a5a6c9e0b76f75901568
Max <75096038+AltronMaxX@users.noreply.github.com>
2023-01-25 23:05:29 +0400

    Update ScriptHelper.hx

--- source/scriptStuff/ScriptHelper.hx
+++ source/scriptStuff/ScriptHelper.hx
@@ -1,11 +1,9 @@
 package scriptStuff;
 
-import scriptStuff.FunkinLua;
 @:allow(states.PlayState)
 class ScriptHelper
 {
 	public static var hscriptFiles:Array<Dynamic> = [];
-	public static var luaArray:Array<FunkinLua> = [];
 
 	public static function clearAllScripts()
 	{
@@ -17,12 +15,10 @@ class ScriptHelper
 	public static function setOnScripts(name:String, value:Dynamic)
 	{
 		setOnHscript(name, value);
-		setOnLuas(name, value);
 	}
 
 	public static function callOnScripts(functionToCall:String, args:Array<Dynamic>):Dynamic {
-		callOnHscript(functionToCall, args);
-		return callOnLuas(functionToCall, args);
+		return callOnHscript(functionToCall, args);
 	}
 
 	public static function setOnHscript(name:String, value:Dynamic)
@@ -58,64 +54,6 @@ class ScriptHelper
 				retVal = true;
 		}
 		return retVal;
-	}
-
-	private static var preventLuaRemove:Bool = false;
-
-	public static function removeLua(lua:FunkinLua)
-	{
-		if (luaArray != null && !preventLuaRemove)
-		{
-			luaArray.remove(lua);
-		}
-	}
-
-	public static var closeLuas:Array<FunkinLua> = [];
-
-	public static function callOnLuas(event:String, args:Array<Dynamic>, ignoreStops = false, ?exclusions:Array<String>):Dynamic
-	{
-		var returnVal:Dynamic = FunkinLua.Function_Continue;
-		if (exclusions == null)
-			exclusions = [];
-		#if LUA_ALLOWED
-		for (i in 0...luaArray.length)
-		{
-			if (exclusions.contains(luaArray[i].scriptName))
-			{
-				continue;
-			}
-			var ret:Dynamic = luaArray[i].call(event, args);
-			if (ret == FunkinLua.Function_StopLua)
-			{
-				if (ignoreStops)
-					ret = FunkinLua.Function_Continue;
-				else
-					break;
-			}
-
-			if (ret != FunkinLua.Function_Continue)
-			{
-				returnVal = ret;
-			}
-		}
-
-		for (i in 0...closeLuas.length)
-		{
-			luaArray.remove(closeLuas[i]);
-			closeLuas[i].stop();
-		}
-		#end
-		return returnVal;
-	}
-
-	public static function setOnLuas(variable:String, arg:Dynamic)
-	{
-		#if LUA_ALLOWED
-		for (i in 0...luaArray.length)
-		{
-			luaArray[i].set(variable, arg);
-		}
-		#end
 	}
 
 	public static function stepHit()

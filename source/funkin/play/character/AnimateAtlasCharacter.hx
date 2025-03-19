3f7115f7ce84495641b684d51d800d9f333a8e91
AltronMaxX <max06112004@gmail.com>
2025-03-19 13:33:07 +0400

    I love error from description
     ERROR  (unknown position)
    
       | Type not found : T

--- source/funkin/play/character/AnimateAtlasCharacter.hx
+++ source/funkin/play/character/AnimateAtlasCharacter.hx
@@ -13,7 +13,6 @@ import funkin.graphics.FunkinSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 import flixel.util.FlxColor;
 import flixel.util.FlxDestroyUtil;
-import flixel.util.FlxDirectionFlags;
 import funkin.graphics.adobeanimate.FlxAtlasSprite;
 import funkin.modding.events.ScriptEvent;
 import funkin.play.character.CharacterData.CharacterRenderType;
@@ -529,10 +528,10 @@ class AnimateAtlasCharacter extends BaseCharacter
     return alpha = value;
   }
 
-  override function set_facing(Direction:FlxDirectionFlags):FlxDirectionFlags
+  override function set_facing(value:Int):Int
   {
-    if (exists && facing != Direction) transformChildren(facingTransform, Direction);
-    return facing = Direction;
+    if (exists && facing != value) transformChildren(facingTransform, value);
+    return facing = value;
   }
 
   override function set_flipX(value:Bool):Bool
